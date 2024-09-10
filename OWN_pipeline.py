c03bcbb54dc2e8c8be7694678428ee8548ffc7c8
Manuela Andrea Beyer <67337290+Miuuu13@users.noreply.github.com>
2024-09-10 08:03:40 +0200

    Create OWN_pipeline.py

---
+++ OWN_pipeline.py
@@ -0,0 +1,21 @@
+
+
+""" Network analysis for one animal id (Batch A or B)"""
+
+
+#%% [1]
+""" Acess data """
+
+# choose based on heatmap animal that has many active neurons at beginning to 
+# have a good starting point for the analysis
+
+
+
+
+
+#%% [2]
+
+
+#%% [3]
+
+#%% [3]
